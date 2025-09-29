ee0f2f98280a2480497d4f2029cd3bee7018c3b8
Longajules <judonite60@gmail.com>
2025-09-29 14:47:01 +0200

    first commit

---
+++ algo.py
@@ -0,0 +1,17 @@
+xA = float(input("donner absicisses de A"))
+
+yA = float(input("donner l'ordonner de A"))
+
+xB = float(input("donner absicisses de B"))
+
+yB = float(input("donner absicisses de B"))
+
+a =(yB-yA)/(xB-xA)
+b=yA-a*xA
+
+print ("y=", a,"x+",b)
+
+
+
+
+
