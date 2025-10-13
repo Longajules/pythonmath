03824faae864c11336cb1e435c05d6d12af7ea7b
Longajules <judonite60@gmail.com>
2025-10-13 14:45:42 +0200

    cointermath

--- seconddegres.py
+++ seconddegres.py
@@ -4,17 +4,24 @@ a = float(input("donner a")) #demande a
 b = float(input("donner b")) #demande b
 c = float(input("donner c")) #demande c
 
-D=b**2-4*a*c
+if a==0:
+    print("formule imposible")
 
-print("D =",D) #affiche delta
+else:
 
-if D<0 :
-    print("pas de solution réelle")
-elif D==0 :
-    print("une solution réelle")
-else :
-    x1=(-b-sqrt(D))/20
-    x2=(-b-sqrt(D))/2*a
 
-    print("la solution x1 est x1=",x1)
-    print("la solution x2 est x2=",x2)
+    D=b**2-4*a*c
+
+    print("D =",D) #affiche delta
+
+    if D<0 :
+        print("pas de solution réelle")
+    elif D==0 :
+        x1=(-b-sqrt(D))/(2*a)
+        print("une solution réelle x1=",x1)
+    else :
+        x1=(-b-sqrt(D))/(2*a)
+        x2=(-b+sqrt(D))/(2*a)
+
+        print("la solution x1 est x1=",x1)
+        print("la solution x2 est x2=",x2)
